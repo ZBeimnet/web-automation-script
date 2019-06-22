1fd35ab8353e18bda4116ea91612af34f2c0360c
Beimnet Zewdu <zbeimnet@gmail.com>
2019-06-22 15:43:48 +0300

    grade reports retrieved from grade report page and printed

--- src/script_for_portal/PortalGradeScript.java
+++ src/script_for_portal/PortalGradeScript.java
@@ -2,26 +2,49 @@ package script_for_portal;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 
 public class PortalGradeScript {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Gecko\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        String url = "https://portal.aait.edu.et";
+        FirefoxDriver driver = new FirefoxDriver();
 
-        driver.get(url);
+        String baseUrl = "https://portal.aait.edu.et";
+        String gradeReportUrl = "https://portal.aait.edu.et/Grade/GradeReport";
+        String myUsername = "ATR/8563/09";
+        String myPassword = "2234";
+
+        driver.get(baseUrl);
 
         //signing into my account
-        driver.findElement(By.id("UserName")).sendKeys("ATR/8563/09");
-        driver.findElement(By.id("Password")).sendKeys("2234");
-        driver.findElement(By.className("btn-success")).click();
+        WebElement usernameTextBox = driver.findElement(By.id("UserName"));
+        WebElement passwordTextBox = driver.findElement(By.id("Password"));
+        WebElement loginButton = driver.findElement(By.className("btn-success"));
+
+        usernameTextBox.sendKeys(myUsername);
+        passwordTextBox.sendKeys(myPassword);
+        loginButton.click();
 
         //go to grade report page
-        driver.navigate().to("https://portal.aait.edu.et/Grade/GradeReport");
+        driver.navigate().to(gradeReportUrl);
 
-//        driver.close();
+        //finding all tr elements in the page
+        List<WebElement> rows = driver.findElements(By.tagName("tr"));
+
+        for (WebElement  row:
+             rows) {
+            for (WebElement td:
+                 row.findElements(By.tagName("td"))) {
+                System.out.print(td.getText() + "  ");
+            }
+            System.out.println();
+        }
+
+        driver.close();
     }
 }
